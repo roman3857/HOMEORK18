10bc17aaeb72a64a47524fc235abeb82c47cedc1
roman3857 <romanshakhrai6@gmail.com>
2026-06-01 21:22:28 +0200

    commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const list = document.querySelector("#categories");
+const listItems = list.children;
+
+console.log(`У списку ${listItems.length} категорії`);
+
+for (const item of listItems) {
+  const title = item.querySelector("h2").textContent;
+  const elementsCount = item.querySelector("ul").children.length;
+
+  console.log(`Категорія: ${title}`);
+  console.log(`Кількість елементів: ${elementsCount}`);
+}
